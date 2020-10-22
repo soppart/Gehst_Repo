da56ae2ecdf089594ead974fff56a67cdc46debd
soppart <soppart@newschool.edu>
2020-10-21 21:29:51 -0500

    bg and plat scripts

---
+++ Assets/Scripts/PlatformUpDown.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformUpDown : MonoBehaviour
+{
+   
+
+    public float speed = .5f;
+    public float distance = 5;
+    private float startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPosition = transform.position;
+        newPosition.y = Mathf.SmoothStep(startPosition, startPosition + distance, Mathf.PingPong(Time.time * speed, 1));
+        transform.position = newPosition;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.transform.position.y > transform.position.y)
+        {
+            other.transform.SetParent(transform);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.SetParent(null);
+        }
+    }
+}
